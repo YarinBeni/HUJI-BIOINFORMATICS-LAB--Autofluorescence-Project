3a27b36b7c1c931aa4ed4c9b3877191f41371bc8
yarin benizri <yarin.benizri@mail.huji.ac.il>
2021-11-22 10:41:00 +0200

    added new todo's and path to dic and florescence

--- Worms_Dataset.py
+++ Worms_Dataset.py
@@ -34,29 +34,30 @@ import glob
 ####################################################
 params = {
     # optional parameters maybe more ?    "model": "U-net","device": "cuda","lr": 0.001,
-    "batch_size": 3,  # todo:the number of image per sample check if need to be changed -A: FOR NOW ITS FINE !
+    "batch_size": 1,  # the number of image per sample check if need to be changed -A: FOR NOW ITS FINE !
     "num_workers": 4,  # todo: relearn whats workers and ask what will be ours -A: itay explained NEED TO GO DEEPER in TRAINING MODEL !
-    "image_size": 256,  # todo: to ask if this need to be the max from worms size padded up square or rectangle and how -A: FORNOW FINE !
-    "in_channels": 1,  # TODO: to make sure because its gray image 1 channel or maybe 3 from tensor size? -A: YES
-    "num_classes": None  # TODO: to make sure we dont have classes because our label is an image -A: EXACTLY !
+    "image_size": 256,  #  to ask if this need to be the max from worms size padded up square or rectangle and how -A: FOR NOW FINE in future will need to be max padded!
+    "in_channels": 1,  # to make sure because its gray image 1 channel or maybe 3 from tensor size? -A: YES
+    "num_classes": None  #  to make sure we dont have classes because our label is an image -A: EXACTLY !
 }
 #######################################################
 #               Define Transforms
 #######################################################
 
-# todo: when i preprocessed the raw-images i used imread default fixture that was color image and now my database images
+#  when i preprocessed the raw-images i used imread default fixture that was color image and now my database images
 #  1)do I need to preprocess them again with flag for gray or we can work from here? - A:NEED TO DO AGAIN CHANGED PIXELS!
 #  2)do I need to add to train transform Compose the function Grayscale(num_output_channels=1) or is imread() with
-#  flag 0 in __Getitem__ is enough?  - A:imread with falg 0 is enough !
+#  flag 0 in __Getitem__ is enough?  - A: imread with falg 0 is enough !
 train_transform = transforms.Compose([transforms.ToTensor()])
-
+# NEW TODO: 1) whats specific transforms i need to uses when using cat(NOT ON COLOUR CHANNEL ON SPACE RELATED)
+# NEW TODO: 2) how to concat horizontal or vertical ?( with flag 0 or 1 torch.cat(image,label))?
 ####################################################
 #       Create Train, Valid and Test sets
 ####################################################
 
-# todo: how to split dataset into train and test sets? in folders structure in advanced or in runtime? - A: datasplit
+# how to split dataset into train and test sets? in folders structure in advanced or in runtime? - A: datasplit
 #  (Random) meanwhile self divided with index (read on torchvision.utils.data random-split) and it happens before dataloaders
-#  todo: to ask if the label of each sample will be additional image in the sample folder for each
+#   to ask if the label of each sample will be additional image in the sample folder for each
 #   sample? ** yarin solution: maybe to get labels folder path parameter and make a folder of labels(images) with
 #   name according to sample name and use make_path_list of labels(images) and to make image_to_label dictionary and
 #   make WormsDataset get the the dictionary as parameter and than to make get__item return (image, label=dic[
@@ -79,19 +80,23 @@ def make_paths_list(data_path):
     path_list = []
     for data_path in glob.glob(data_path + "/*"):
         path_list.append(glob.glob(data_path + "/*"))
-    path_list = list(flatten(path_list))
-    return path_list
+    mixed_images = list(flatten(path_list))
+    # todo: this is maybe problem because its dependet on the order of glob i can take only adress of dic and use
+    #  replace do find the label in the folder regardless of order, it may be costly but can work (can run here on dic_paths or in every call to get
+    dic_path_list = [dic_image for dic_image in mixed_images if dic_image.endswith("(DIC)_M0000_ORG.tif")]
+    floresence_path_list = [floresence_image for floresence_image in mixed_images if floresence_image.endswith("(mCher)_M0000_ORG.tif")]
+    return dic_path_list, floresence_path_list
 
 
-train_image_paths = make_paths_list(TRAIN_DATASET_PATH)
+dic_path_list, floresence_path_list  = make_paths_list(TRAIN_DATASET_PATH)
 
 
-# todo: ask if need to shuffle the paths of images - A: no need will be happening in the dataloader
+# ask if need to shuffle the paths of images - A: no need will be happening in the dataloader
 # random.shuffle(train_image_paths)
 
 def show_dataset_paths(paths):
     """this 4 lines is to see whats the len and paths of the dataset."""
-    n = 3  # n is the fixed number of images per sample
+    n = 1  # n is the fixed number of images per sample
     print("this is number of paths of images in the dataset: \n", len(paths))
     for i in range(len(paths)):
         print('train_image_path index:{}: \n'.format(i), paths[i])
@@ -99,7 +104,7 @@ def show_dataset_paths(paths):
             print()
 
 
-# if want to print delete and UN-comma: show_dataset_paths(train_image_paths)
+#show_dataset_paths(train_image_paths)
 
 
 # from here until row 110 are things mayby will be in use according to solution to the to do up top.
@@ -115,25 +120,25 @@ def show_dataset_paths(paths):
 
 
 class WormsDataset(Dataset):
-    def __init__(self, image_paths, transform=None):
-        self.image_paths = image_paths
+    def __init__(self, image_paths,labels_paths, transform=None):
+        self.dic_images_paths = image_paths
+        self.floresence_images_paths = labels_paths
         self.transform = transform
 
     def __len__(self):
-        return len(self.image_paths)
+        return len(self.dic_images_paths)
 
-    def __getitem__(self, index):  # todo: ask if its ok i am running over database class __getitem__ func? -A: its fine.
-        image_filepath = self.image_paths[index]
-
-        # todo: ask why cv2.imread() not referenced? saw pycharm error, and its working !-A:  Seems fine ignore it !
-        image = cv2.imread(image_filepath, 0)  # 0 is grey flag
-
+    def __getitem__(self, index):  # ask if its ok i am running over database class __getitem__ func? -A: its fine.
+        dic_image_filepath = self.dic_images_paths[index]
+        florescence_image_path = self.floresence_images_paths[index]
+        # ask why cv2.imread() not referenced? saw pycharm error, and its working !-A:  Seems fine ignore it !
+        dic_image = cv2.imread(dic_image_filepath, 0)  # 0 is grey flag
+        florescence_image = cv2.imread(florescence_image_path, 0)
         if self.transform:
-            image = self.transform(image)
-        y_label = image  # todo: temporary until labels structure is solved, than change it to real label -
-        # A: need to build a function that read DIC or florescence picture and dic is the image and florescence
-        # is the label
-        return image, y_label
+            dic_image = self.transform(dic_image)
+            #NEW TODO: florescence_image = self.transform(florescence_image) to do this or to do concat and than?
+        # NEW TODO: this label is mcher image in future will be florescence
+        return dic_image, florescence_image
 
 
 #######################################################
@@ -141,51 +146,14 @@ class WormsDataset(Dataset):
 #######################################################
 
 
-train_dataset = WormsDataset(train_image_paths, train_transform)
-
+train_dataset = WormsDataset(dic_path_list, floresence_path_list, train_transform)
+# x=train_dataset.__getitem__(0)
+# print("this is dic\n",x[0])
+# print()
+# print("this is floresence\n",x[1])
 
 # valid_dataset = WormsDataset(valid_image_paths,test_transforms) #test transforms are applied
 # test_dataset = WormsDataset(test_image_paths,test_transforms)
-
-
-#######################################################
-#                  Visualize Dataset
-#         Images are plotted after augmentation
-#######################################################
-
-def show_images_one_by_one(dataset):
-    """get the dataset object of images and print them one by one"""
-    # print images one-by one from a dataset object
-    # cnt = 1
-    for sample in dataset:
-        # print("this is iter number: ", cnt)
-        # lst.append(image)
-        image, label = sample
-        plt.imshow(image.squeeze(), cmap="gray")
-        plt.show()
-    #     cnt += 1
-    # print("this is number of images showed :",cnt)
-    plt.close()
-
-
-# todo: need to change to show only one grid with next iter or to fix a batch[0] in end of epoch to show
-def show_in_grid(images_iter):
-    """gets dataloader object and print in one grid all images of same sample"""
-    for batch in images_iter:
-        # for batch in dataloader:
-        # batch = next(iter(dataloader))
-        images, labels = batch
-        grid = torchvision.utils.make_grid(images, nrow=3)  # padding=0,nrow and padding variables need to be adjust
-        plt.figure(figsize=(15, 15))
-        plt.imshow(np.transpose(grid, (1, 2, 0)))
-        plt.show()
-
-
-# UN COMMENT the action you desired:
-# #this will show in one grid all 3 samples of same image
-# show_in_grid(train_loader)
-# #this will show all images one by one
-# show_images_one_by_one(train_dataset)
 
 
 #######################################################
@@ -201,4 +169,43 @@ train_loader = DataLoader(train_dataset, batch_size=params["batch_size"], shuffl
 #     test_dataset, batch_size=params["batch_size"], shuffle=False
 # )
 
+
+
+#######################################################
+#                  Visualize Dataset
+#         Images are plotted after augmentation
+#######################################################
+
+def show_images_one_by_one(dataset):
+    """get the dataset object of images and print them one by one"""
+    # print images one-by one from a dataset object
+    # cnt = 1
+    for sample in dataset:
+        # print("this is iter number: ", cnt)
+        # lst.append(image)
+        dic_image, florescence_image = sample
+        plt.imshow(dic_image.squeeze(), cmap="gray")
+        plt.show()
+        plt.imshow(florescence_image.squeeze(), cmap="gray")
+        plt.show()
+    #     cnt += 1
+    # print("this is number of images showed :",cnt)
+    plt.close()
+
+
+# todo: need to change to show only one grid with next iter or to fix a batch[0] in end of epoch to show
+def show_in_grid(images_iter):
+    """gets dataloader object and print in one grid all images of same sample"""
+    for batch in images_iter:
+        # batch = next(iter(dataloader))
+        images, labels = batch
+        grid = torchvision.utils.make_grid(images, nrow=2)  # padding=0,nrow and padding variables need to be adjust
+        plt.figure(figsize=(15, 15))
+        plt.imshow(np.transpose(grid, (1, 2, 0)))
+        plt.show()
+
+# #this will show in one grid all 3 samples of same image
+show_in_grid(train_loader)
+# #this will show all images one by one
+#show_images_one_by_one(train_loader) #WORKS BETTER WITH COLOUR
 
